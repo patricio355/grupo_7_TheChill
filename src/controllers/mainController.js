fd16caf7b417c9d4ea5d8205b672cace7c93720c
OneLean <leandro.montellano@gmail.com>
2023-08-31 10:10:43 -0300

    Subo images para testear

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
+
+const productsFilePath = path.join(__dirname, "../data/products.json");
+const products = JSON.parse(fs.readFileSync(productsFilePath, "utf-8"));
 
 const mainController = {
     showHome: (req,res)=>{
-        res.render(path.join(__dirname,"../views/home/index.ejs"));
+        res.render(path.join(__dirname,"../views/home/index.ejs"),{ products });
     }
 }
 
