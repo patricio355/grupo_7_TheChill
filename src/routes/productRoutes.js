ccc2769cec6f976c6dd5fcde1b08de5b2f6111c3
Ags-99 <paredesagustin477@gmail.com>
2023-09-01 10:44:28 -0300

    subiendo productDetail

--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -3,7 +3,7 @@ const productRoutes = express.Router();
 
 const productControllers = require("../controllers/productControllers.js")
 
-productRoutes.get('/productodetalle', productControllers.productDetail);
+productRoutes.get('/productDetail/:id', productControllers.productDetail);
 productRoutes.get('/carrito', productControllers.productCart);
 
 module.exports = productRoutes
