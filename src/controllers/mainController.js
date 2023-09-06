87ba5e0e20b286695f5ae88ae9d12508b64dd063
patricio355 <patricioquiroga35@gmail.com>
2023-09-06 14:02:26 -0300

    arreglando carrousel

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -2,6 +2,10 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 
+
+
+
+
 const productsFilePath = path.join(__dirname, "../data/products.json");
 const products = JSON.parse(fs.readFileSync(productsFilePath, "utf-8"));
 
