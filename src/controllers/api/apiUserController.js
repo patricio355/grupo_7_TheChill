0982d241ad5705ac402ece79420bb1c23f88c621
mmarifuentes <maria.fuentes1203@gmail.com>
2023-12-12 10:02:14 -0300

    api de usuarios

--- src/controllers/api/apiUserController.js
+++ src/controllers/api/apiUserController.js
@@ -3,21 +3,61 @@ const db = require('../../../database/models');
 
 module.exports={
     list: async (req,res) => {
-        const data = await db.User.findAll();
-        
-        res.json({
-            code: 200,
-            total: data.length,
-            users: data
-        });
+        try {
+		const data = await db.User.findAll({
+			attributes: ['id', 'first_name','last_name', 'email'],
+		});
+
+		const count = data.length;
+
+		const usersArray = data.map(user => ({
+			id: user.id,
+			first_name: user.first_name,
+            last_name: user.last_name,
+			email: user.email,
+			detail: `http://localhost:8000/api/users/${user.id}`,
+		}));
+
+		res.json({
+			count,
+			users: usersArray,
+		});
+	} catch (error) {
+		console.error(error);
+		res.status(500).json({
+			error: 'Error al obtener la lista de usuarios.',
+		});
+	} 
        
     },
 
     show: async (req,res) =>{
-        const data = await db.User.findByPk(req.params.id);   
-        res.json({
-            code: 200,
-            user: data
-        });
+        try {
+
+		const data = await db.User.findByPk(req.params.id , {
+			attributes: { exclude: ['passwordHash'] }, 	
+		});
+
+		if (!data) {
+			return res.status(404).json({ error: 'Usuario no encontrado.' });
+		}
+
+		const userData = {
+			id: data.id,
+			first_name: data.first_name,
+            last_name: data.last_name,
+            gender: data.gender,
+			email: data.email,
+			image: data.avatar,
+		};
+
+		res.json(userData);
+	} catch (error) {
+		console.error(error);
+		res.status(500).json({
+			error: 'Error al obtener información del usuario.',
+		});
+	} 
+
     }
 }
