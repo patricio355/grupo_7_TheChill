75a7f66d44bc4465283de9846d181ba558f54b0a
LeanM <leandro.montellano@gmail.com>
2023-12-12 19:02:44 -0300

    Modificacion de la BD para que cada usuario tenga su carrito

--- structure.sql
+++ structure.sql
@@ -15,77 +15,6 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
---
--- Table structure for table `cart`
---
-
-DROP TABLE IF EXISTS `cart`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `cart` (
-  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-  `sessionId` varchar(100) DEFAULT NULL,
-  `token` varchar(100) DEFAULT NULL,
-  `status` smallint(6) DEFAULT NULL,
-  `first_name` varchar(100) DEFAULT NULL,
-  `middleName` varchar(50) DEFAULT NULL,
-  `last_name` varchar(100) DEFAULT NULL,
-  `mobile` varchar(15) DEFAULT NULL,
-  `email` varchar(15) DEFAULT NULL,
-  `city` varchar(50) DEFAULT NULL,
-  `province` varchar(50) DEFAULT NULL,
-  `country` varchar(50) DEFAULT NULL,
-  `createdAt` datetime DEFAULT NULL,
-  `updatedAt` datetime DEFAULT NULL,
-  `content` text DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `cart`
---
-
-LOCK TABLES `cart` WRITE;
-/*!40000 ALTER TABLE `cart` DISABLE KEYS */;
-/*!40000 ALTER TABLE `cart` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `cart_item`
---
-
-DROP TABLE IF EXISTS `cart_item`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `cart_item` (
-  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-  `sku` varchar(100) DEFAULT NULL,
-  `price` float NOT NULL,
-  `discount` float DEFAULT NULL,
-  `quantity` smallint(6) DEFAULT NULL,
-  `active` tinyint(1) DEFAULT NULL,
-  `createdAt` datetime DEFAULT NULL,
-  `updatedAt` datetime DEFAULT NULL,
-  `content` text DEFAULT NULL,
-  `productId` bigint(20) unsigned NOT NULL,
-  `cartId` bigint(20) unsigned NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `cart_item_productId_foreign` (`productId`),
-  KEY `cart_item_cartId_foreign` (`cartId`),
-  CONSTRAINT `cart_item_cartId_foreign` FOREIGN KEY (`cartId`) REFERENCES `cart` (`id`),
-  CONSTRAINT `cart_item_productId_foreign` FOREIGN KEY (`productId`) REFERENCES `product` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `cart_item`
---
-
-LOCK TABLES `cart_item` WRITE;
-/*!40000 ALTER TABLE `cart_item` DISABLE KEYS */;
-/*!40000 ALTER TABLE `cart_item` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `category`
@@ -446,6 +375,80 @@ LOCK TABLES `user` WRITE;
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+--
+-- Table structure for table `cart`
+--
+
+DROP TABLE IF EXISTS `cart`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `cart` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `userId` bigint(20) unsigned NOT NULL,
+  `sessionId` varchar(100) DEFAULT NULL,
+  `token` varchar(100) DEFAULT NULL,
+  `status` smallint(6) DEFAULT NULL,
+  `first_name` varchar(100) DEFAULT NULL,
+  `middleName` varchar(50) DEFAULT NULL,
+  `last_name` varchar(100) DEFAULT NULL,
+  `mobile` varchar(15) DEFAULT NULL,
+  `email` varchar(15) DEFAULT NULL,
+  `city` varchar(50) DEFAULT NULL,
+  `province` varchar(50) DEFAULT NULL,
+  `country` varchar(50) DEFAULT NULL,
+  `createdAt` datetime DEFAULT NULL,
+  `updatedAt` datetime DEFAULT NULL,
+  `content` text DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (`userId`) REFERENCES `user` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `cart`
+--
+
+LOCK TABLES `cart` WRITE;
+/*!40000 ALTER TABLE `cart` DISABLE KEYS */;
+/*!40000 ALTER TABLE `cart` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `cart_item`
+--
+
+DROP TABLE IF EXISTS `cart_item`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `cart_item` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `sku` varchar(100) DEFAULT NULL,
+  `price` float NOT NULL,
+  `discount` float DEFAULT NULL,
+  `quantity` smallint(6) DEFAULT NULL,
+  `active` tinyint(1) DEFAULT NULL,
+  `createdAt` datetime DEFAULT NULL,
+  `updatedAt` datetime DEFAULT NULL,
+  `content` text DEFAULT NULL,
+  `productId` bigint(20) unsigned NOT NULL,
+  `cartId` bigint(20) unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `cart_item_productId_foreign` (`productId`),
+  KEY `cart_item_cartId_foreign` (`cartId`),
+  CONSTRAINT `cart_item_cartId_foreign` FOREIGN KEY (`cartId`) REFERENCES `cart` (`id`),
+  CONSTRAINT `cart_item_productId_foreign` FOREIGN KEY (`productId`) REFERENCES `product` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `cart_item`
+--
+
+LOCK TABLES `cart_item` WRITE;
+/*!40000 ALTER TABLE `cart_item` DISABLE KEYS */;
+/*!40000 ALTER TABLE `cart_item` ENABLE KEYS */;
+UNLOCK TABLES;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
